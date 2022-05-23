fe2b4f00f149c677515a06c7da416c5ef76bdc9d
kendricktty <kendricktty@outlook.com>
2022-05-23 12:10:40 +0800

    Updated ExpressJS tes code

---
+++ src/testexpress.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+ 
+app.use(express.static('public'));
+const PORT = process.env.PORT || 4001;
+
+const names = ["A", "B", "C", "D", "E"];
+
+app.get('/hello', (req, res, next) => {
+    res.send("Hello world!");
+});
