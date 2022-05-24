fb409c820068d46dafa85c19230b7c3a0c4c96f7
Jing kai Ooi <ooijingkai@gmail.com>
2022-05-25 07:23:51 +0800

    Initial Commit with backend prototype

---
+++ inventoryapp-frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Nav1 from './Nav.js'
+
+function App() {
+  return (
+   <>
+   <Nav1 />
+   <button className="btn btn-lg btn-outline-dark">Helllo</button>
+   </>
+  )
+}
+
+export default App;
