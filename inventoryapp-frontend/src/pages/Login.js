f3d38bfa00ad1d2adace5db96c9266b0f3b58950
Yi Peng <tanyipeng835@hotmail.com>
2022-05-29 01:58:10 +0800

    Added Login Page

--- inventoryapp-frontend/src/pages/Login.js
+++ inventoryapp-frontend/src/pages/Login.js
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import styles from "../styles/login.module.css";
 
 export default function Login() {
-    return(
-        <div className="login">
-            <h2>Welcome to InventoryApp</h2>
-            <a href="/inventory">Inventory</a>
+  return (
+    <div className={"container-fluid" + " " + styles.login}>
+      <h2 className="text-center">Welcome to Inventory App</h2>
+      <div
+        className={"container align-self-center gy-4 mt-5" + " " + styles.form}
+      >
+        <div className="row d-flex justify-content-center">
+          <div className="col-4 text-center">
+            <img
+              className="img-fluid"
+              src="../../logo192.png"
+              width="30%"
+              height="auto"
+            ></img>
+          </div>
+          <div className="row d-flex justify-content-center">
+            <div className="col-6 px-3">
+              Login:
+              <input type="text" className="form-control"></input>
+            </div>
+            <div className="row d-flex justify-content-center">
+              <div className="col-6 ">
+                Password:
+                <input type="password" className="form-control "></input>
+              </div>
+              <div className="row d-flex justify-content-center">
+                <div className="col-4 text-center">
+                  <button type="button " className="btn btn-success mt-3 ">
+                    <a
+                      style={{ color: "white", textDecoration: "none" }}
+                      href="/inventory"
+                    >
+                      Login
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        
-    )
+      </div>
+    </div>
+  );
 }
