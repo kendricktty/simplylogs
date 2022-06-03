23b94d53896021284e5588214e1a482fca712d1d
Yi Peng <tanyipeng835@hotmail.com>
2022-06-03 18:48:35 +0800

    added working form

--- inventoryapp-frontend/src/components/InventoryUtilityBar.js
+++ inventoryapp-frontend/src/components/InventoryUtilityBar.js
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Form,
+  Button,
+  FormGroup,
+  FormControl,
+  ControlLabel,
+} from "react-bootstrap";
 
 export default function InventoryUtilityBar(props) {
-    // const handleSubmit = event => {
-    //     event.preventDefault();
-    //     alert('You have submitted the form.')
-    //     setSearch('')
-    // }
+  // const handleSubmit = event => {
+  //     event.preventDefault();
+  //     alert('You have submitted the form.')
+  //     setSearch('')
+  // }
 
-    // const [search, setSearch] = React.useState('')
+  // const [search, setSearch] = React.useState('')
+  // This is the function when the button is pressed
+  // to add items to data.json
 
+  const [showForm, setForm] = React.useState(false);
+  function addButtonPressed() {
+    console.log(showForm);
+    console.log("Pressed");
+    setForm(true);
+  }
 
-
-    return (
-        <div className="InventoryUtilityBar row">
-            {/* <div class="row select-view col-lg-4 col-sm-12">
+  return (
+    <div className="InventoryUtilityBar row">
+      {/* <div class="row select-view col-lg-4 col-sm-12">
                 <div class="col-lg-12 my-3">
                     <div class="pull-right">
                         <div class="btn-group">
@@ -27,21 +42,73 @@ export default function InventoryUtilityBar(props) {
                     </div>
                 </div>
             </div> */}
-            <form className="searchBar col-lg-4 col-sm-12 my-3" action="" method="GET" >
-                <div class="input-group">
-                    <input 
-                    type="text" class="form-control rounded" id="search"
-			
-			placeholder="Filter By Name"
-			aria-label="Search Input"
-			value={props.filterText}
-			onChange={props.onFilter}/>
-                    <button type="button" onClick={props.onClear} class="btn btn-outline-primary" >Clear</button>
-                </div>
-            </form>
-            <div className="col-lg-4"></div>
-            <button class="btn btn-outline-primary col-lg-2 col-sm-6 utilityBtn my-3">Export</button>
-            <button class="btn btn-outline-primary col-lg-2 col-sm-6 utilityBtn my-3">Add +</button>
+      <form
+        className="searchBar col-lg-4 col-sm-12 my-3"
+        action=""
+        method="GET"
+      >
+        <div class="input-group">
+          <input
+            type="text"
+            class="form-control rounded"
+            id="search"
+            placeholder="Filter By Name"
+            aria-label="Search Input"
+            value={props.filterText}
+            onChange={props.onFilter}
+          />
+          <button
+            type="button"
+            onClick={props.onClear}
+            class="btn btn-outline-primary"
+          >
+            Clear
+          </button>
         </div>
-    )
+      </form>
+      <div className="col-lg-4"></div>
+      <button class="btn btn-outline-primary col-lg-2 col-sm-6 utilityBtn my-3">
+        Export
+      </button>
+      <button
+        class="btn btn-outline-primary col-lg-2 col-sm-6 utilityBtn my-3"
+        onClick={() => addButtonPressed()}
+      >
+        Add +
+      </button>
+
+      {showForm ? (
+        <Form>
+          <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+            <Form.Label>ProductID</Form.Label>
+            <Form.Control type="text" placeholder="ProductId" />
+          </Form.Group>
+          <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+            <Form.Label>Product Name</Form.Label>
+            <Form.Control type="text" placeholder="Product Name" />
+          </Form.Group>
+          <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+            <Form.Label>Supplier</Form.Label>
+            <Form.Control type="text" placeholder="Product Supplier" />
+          </Form.Group>
+          <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+            <Form.Label>Quanity</Form.Label>
+            <Form.Control type="number" placeholder="Quantity" />
+          </Form.Group>
+          <Form.Group className="mb-1" controlId="exampleForm.ControlInput1">
+            <Form.Label>Price</Form.Label>
+            <Form.Control type="number" placeholder="Price($)" />
+          </Form.Group>
+          <Button
+            variant="primary"
+            onClick={() => {
+              setForm(false);
+            }}
+          >
+            Submit
+          </Button>
+        </Form>
+      ) : null}
+    </div>
+  );
 }
