5bf8c24acacf96dd9c2d1fa4c944cf59bcf1fa53
kendricktty <79306624+kendricktty@users.noreply.github.com>
2022-07-07 12:10:01 +0800

    Completed Products controller

--- inventoryapp-backend/controllers/orders.js
+++ inventoryapp-backend/controllers/orders.js
@@ -1,13 +1,19 @@
 const Order = require('../model/order')
 
 const getAllOrders = async (req, res, next) => {
-    const orders = await Order.find({})
+    const orders = await Order.find({});
     res.status(200).json({ orders })
+    console.log('Get all orders');
 }
 
 const addOrder = async (req, res, next) => {
     const order = await Order.create(req.body)
     res.status(201).json({ order })
+    console.log('Add order');
+}
+
+const getOrder = async (req, res, next) => {
+    
 }
 
 
