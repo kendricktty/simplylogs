41689f86b8efd4b1d0e1d02d65990a0de812f71b
kendricktty <79306624+kendricktty@users.noreply.github.com>
2022-07-07 12:11:27 +0800

    Revert "Completed Products controller"
    This reverts commit 5bf8c24acacf96dd9c2d1fa4c944cf59bcf1fa53.

--- inventoryapp-backend/middleware/error-handler.js
+++ inventoryapp-backend/middleware/error-handler.js
@@ -1,9 +1,7 @@
 const errorHandler = async (err, req, res, next) => {
     if (err.status === 404) {
         console.log(err.status);
-        console.log(err.message);
         return res.json({
-            status: err.status,
             msg: err.msg
         });
     }
