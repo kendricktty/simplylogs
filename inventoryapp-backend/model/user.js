f3d5eff39d401ef40c885d722ce50fcb5ca479bb
Jing kai Ooi <ooijingkai@gmail.com>
2022-07-10 10:16:20 +0800

    User model created

--- inventoryapp-backend/model/user.js
+++ inventoryapp-backend/model/user.js
@@ -1,26 +1,44 @@
 const mongoose = require('mongoose')
-const {productSchema} = require('./product')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-const userSchema = mongoose.Schema({
-    userid: {
+const userSchema = new mongoose.Schema({
+    name: {
         type: String,
-        required: [true, "Please enter a memorable user ID"]
+        required: [true, 'Please provide name'],
+        minLength: 3,
+        maxLength: 50,
     },
-    fullName: {
+    email: {
         type: String,
-        required: [true, "Enter your full name"]
-    },
-    emailAddress: {
-        type: String,
-        required: [true, "Enter an email address"]
+        required: [true, 'Please provide email'],
+        minLength: 3,
+        maxLength: 50,
+        match: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please provide valid email,'],
+        unique: true,
     },
     password: {
         type: String,
-        required: [true, "Enter a password"]
+        required: [true, 'Please provide password'],
+        minLength: 6,
     },
-    salt: String,
-    inventory: [productSchema],
-    orders: []
-});
+})
 
-module.exports = mongoose.model('user', userSchema)
+userSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+})
+
+userSchema.methods.createJWT = function () {
+    return jwt.sign({ userId: this._id, name: this.name }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_LIFETIME
+    })
+}
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password)
+    return isMatch
+}
+
+module.exports = mongoose.model('User', userSchema)
