a279f2bf5b42b1529c6e42ce7275a3b288b6fb35
Yi Peng <tanyipeng835@hotmail.com>
2022-07-12 15:52:34 +0800

    Building Login Page

--- inventoryapp-frontend/src/pages/Login.js
+++ inventoryapp-frontend/src/pages/Login.js
@@ -1,45 +1,59 @@
 import React from "react";
 import styles from "../styles/login.module.css";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function Login() {
   return (
     <div className={"container-fluid" + " " + styles.login}>
-      <h2 className="text-center">Welcome to Inventory App</h2>
-      <div
-        className={"container align-self-center gy-4 mt-5" + " " + styles.form}
-      >
-        <div className="row d-flex justify-content-center">
-          <div className="col-4 text-center">
+      <div className={"row" + " " + styles.login}>
+        <div className={"col-md-6" + " " + styles.leftSide}>
+          <Carousel
+            controls={false}
+            indicators={false}
+            interval={2000}
+            className="mt-5"
+          >
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="../../undraw_finance_re.svg"
+                alt="First slide"
+              />
+
+              <h3 className={styles.carousellItem}>Accounting</h3>
+              <p className={styles.carousellItem}>Need Help With Accounting?</p>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="../../undraw_logistics.svg"
+                alt="Second Slide"
+              />
+
+              <h3 className={styles.carousellItem}>Logistics</h3>
+              <p className={styles.carousellItem}>Need Help With Logisitcs?</p>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="../../undraw_term_sheet.svg"
+                alt="Third Slide"
+              />
+
+              <h3 className={styles.carousellItem}>Workflow Management</h3>
+              <p className={styles.carousellItem}>
+                Need Help with streamlining your workflow proccesses?
+              </p>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className={"col-md-6" + " " + styles.rightSide}>
+          <div>
+            <h1 className={styles.logo + " " + "mt-3"}>SIMPLY LOGS</h1>
             <img
-              className="img-fluid"
-              src="../../logo192.png"
-              width="30%"
-              height="auto"
-            ></img>
-          </div>
-          <div className="row d-flex justify-content-center">
-            <div className="col-6 px-3">
-              Login:
-              <input type="text" className="form-control"></input>
-            </div>
-            <div className="row d-flex justify-content-center">
-              <div className="col-6 ">
-                Password:
-                <input type="password" className="form-control "></input>
-              </div>
-              <div className="row d-flex justify-content-center">
-                <div className="col-4 text-center">
-                  <button type="button " className="btn btn-success mt-3 ">
-                    <a
-                      style={{ color: "white", textDecoration: "none" }}
-                      href="/dashboard"
-                    >
-                      Login
-                    </a>
-                  </button>
-                </div>
-              </div>
-            </div>
+              src="../../logo.jpg"
+              style={{ width: "125px", height: "auto" }}
+            />
           </div>
         </div>
       </div>
