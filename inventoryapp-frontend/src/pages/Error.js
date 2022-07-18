0087fdab47878dd9aa4261e6da7005c85b0f3ab8
Yi Peng <tanyipeng835@hotmail.com>
2022-07-18 23:30:19 +0800

    add error msg

--- inventoryapp-frontend/src/pages/Error.js
+++ inventoryapp-frontend/src/pages/Error.js
@@ -1,29 +1,19 @@
 import styles from "../styles/error.module.css";
+import React from "react";
 
 export default function Error() {
   return (
-    <section class="page_404">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 ">
-            <div class="col-sm-10 col-sm-offset-1  text-center">
-              <div class="four_zero_four_bg">
-                <h1 class="text-center ">404</h1>
-              </div>
-
-              <div class="contant_box_404">
-                <h3 class="h2">Look like you're lost</h3>
-
-                <p>the page you are looking for not avaible!</p>
-
-                <a href="" class="link_404">
-                  Go to Home
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className={"container-fluid" + " " + styles.background}>
+      <div className="row text-center">
+        <form action="/register">
+          <h2 className={styles.error}>Error 404</h2>
+          <p className={styles.textBody}>Please Redirect to our Main Page</p>
+          <div className={"col-12 text-center" + " " + styles.gif}></div>
+          <button className={styles.button} type="submit">
+            Return Back to HomePage
+          </button>
+        </form>
       </div>
-    </section>
+    </div>
   );
 }
