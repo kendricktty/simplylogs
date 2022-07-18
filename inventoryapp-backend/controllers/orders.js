a632859846d62b8652c01c753117ee59acecc348
zdwong9 <zdwong.2021@scis.smu.edu.sg>
2022-07-18 12:50:53 +0800

    Made Dashboard API Calls

--- inventoryapp-backend/controllers/orders.js
+++ inventoryapp-backend/controllers/orders.js
@@ -1,126 +1,216 @@
-const Order = require('../model/order')
-const { BadRequestError } = require('../errors')
+const Order = require("../model/order");
+const { BadRequestError } = require("../errors");
 
 const month = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
 ];
 
 function getTotal(orders) {
-    let total = 0
-    for (let index = 0; index < orders.length; index++) {
-        total += orders[index].grossTotal;
-    }
-    return total
+  //reduce the order array to a single value
+  return orders.reduce((sum, order) => (sum += order.grossTotal), 0);
 }
 
 const getAllOrders = async (req, res) => {
-    const { period, get12MonthsData } = req.query
-    const queryObject = {}
-    queryObject.company = req.user.company
+  const { period, get12MonthsData } = req.query;
+  const queryObject = {};
+  queryObject.company = req.user.company;
 
-    const today = new Date().toLocaleDateString('en-GB', { timeZone: 'Singapore' })
+  const today = new Date().toLocaleDateString("en-GB", {
+    timeZone: "Singapore",
+  });
 
-    const dateArr = today.split('/')
+  const dateArr = today.split("/");
 
-    const sgtISOString = dateArr[2] + "-" + dateArr[1] + "-" + dateArr[0] + "T00:00:00.000+08:00"
-    const startOfDay = new Date(sgtISOString)
+  const sgtISOString =
+    dateArr[2] + "-" + dateArr[1] + "-" + dateArr[0] + "T00:00:00.000+08:00";
+  const startOfDay = new Date(sgtISOString);
 
-    const sgtStartOfMonthISO = dateArr[2] + "-" + dateArr[1] + "-" + "01T00:00:00.000+08:00"
-    const startOfMonth = new Date(sgtStartOfMonthISO)
-    const day = new Date().getDay()
+  const sgtStartOfMonthISO =
+    dateArr[2] + "-" + dateArr[1] + "-" + "01T00:00:00.000+08:00";
+  const startOfMonth = new Date(sgtStartOfMonthISO);
+  const day = new Date().getDay();
 
-    if(period === 'daily') {
-        queryObject.createdAt = { $gte: startOfDay.toISOString()}
-    }
+  const previousDay = new Date(startOfDay.getTime() - (day - 1) * 86400000);
+  const previousWeek = new Date(startOfDay.getTime() - (day + 6) * 86400000);
+  const previousMonth = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() - 1,
+    1
+  );
+  previousMonth.setDate(previousMonth.getDate() + 1);
 
-    if(period === 'weekly') {
-        // console.log(startOfDay.toISOString())
-        // console.log(new Date(startOfDay.getTime() - (day - 1) * 86400000).toISOString());
-        queryObject.createdAt = { $gte: new Date(startOfDay.getTime() - (day - 1) * 86400000).toISOString()}
-    }
+  const previousPreviousDay = new Date(previousDay);
+  previousPreviousDay.setDate(previousDay.getDate() - 1);
 
-    if(period === 'month') {
-        queryObject.createdAt = { $gte: startOfMonth.toISOString()}
-    }
+  const previousPreviousWeek = new Date(previousWeek);
+  previousPreviousWeek.setDate(previousWeek.getDate() - 8);
 
+  const previousPreviousMonth = new Date(
+    previousMonth.getFullYear(),
+    previousMonth.getMonth() - 1,
+    1
+  );
+  previousPreviousMonth.setDate(previousPreviousMonth.getDate() + 1);
+
+  if (
+    period &&
+    period !== "daily" &&
+    period !== "weekly" &&
+    period !== "monthly"
+  ) {
+    throw new BadRequestError("Wrong value for period");
+  }
+
+  const monthlyRevenue = [];
+  if (get12MonthsData) {
     //substract month function
     function subtractMonths(numOfMonths, date) {
-        date.setMonth(date.getMonth() - numOfMonths);
-        return date;
+      date.setMonth(date.getMonth() - numOfMonths);
+      return date;
     }
 
     //add month function
     function addMonths(numOfMonths, date) {
-        const tempDate = date
-        tempDate.setMonth(date.getMonth() + numOfMonths);
-        return tempDate;
+      const tempDate = date;
+      tempDate.setMonth(date.getMonth() + numOfMonths);
+      return tempDate;
     }
 
+    let StartMonth = startOfMonth;
 
+    const EndMonth = addMonths(1, new Date(StartMonth.toISOString()));
 
-    const monthlyRevenue = []
-    if(get12MonthsData) {
+    const orders = await Order.find({
+      createdAt: {
+        $gte: StartMonth.toISOString(),
+        $lt: EndMonth.toISOString(),
+      },
+      company: req.user.company,
+    });
 
-        let StartMonth = startOfMonth
+    const dataObject = {
+      name: month[StartMonth.getMonth()],
+      Revenue: getTotal(orders),
+    };
 
-        const EndMonth = addMonths(1, new Date(StartMonth.toISOString()))
+    monthlyRevenue.push(dataObject);
 
-        const orders = await Order.find({createdAt : {$gte:StartMonth.toISOString(), $lt:EndMonth.toISOString()}, company: req.user.company})
+    for (let index = 1; index < 12; index++) {
+      StartMonth = subtractMonths(1, StartMonth);
 
-        const dataObject = { name:month[StartMonth.getMonth()], "Revenue": getTotal(orders)}
+      const EndMonth = addMonths(1, new Date(StartMonth.toISOString()));
 
-        monthlyRevenue.push(dataObject)
+      const orders = await Order.find({
+        createdAt: {
+          $gte: StartMonth.toISOString(),
+          $lt: EndMonth.toISOString(),
+        },
+        company: req.user.company,
+      });
 
-        for (let index = 1; index < 12; index++) {
-            
-            StartMonth = subtractMonths(1,StartMonth)
-            
-            const EndMonth = addMonths(1, new Date(StartMonth.toISOString()))
+      const dataObject = {
+        name: month[StartMonth.getMonth()],
+        Revenue: getTotal(orders),
+      };
 
-            const orders = await Order.find({createdAt : {$gte:StartMonth.toISOString(), $lt:EndMonth.toISOString()}, company: req.user.company})
-
-            const dataObject = { name:month[StartMonth.getMonth()], "Revenue": getTotal(orders)}
-
-            monthlyRevenue.push(dataObject)
-        }
+      monthlyRevenue.push(dataObject);
     }
+  }
 
+  const orders = await Order.find(queryObject);
 
-    const orders = await Order.find(queryObject)
+  const previousPeriod =
+    period === "daily"
+      ? previousDay
+      : period === "weekly"
+      ? previousWeek
+      : previousMonth;
 
+  const previousPreviousPeriod =
+    period === "daily"
+      ? previousPreviousDay
+      : period === "weekly"
+      ? previousPreviousWeek
+      : previousPreviousMonth;
 
-    
-    res.status(200).json({ orders, orderCount: orders.length, revenue: getTotal(orders),monthlyRevenue })
-}
+  const filteredPreviousPeriodOrders = await orders.filter(
+    order => new Date(order.createdAt) >= previousPeriod
+  );
+
+  const filteredPreviousPreviousPeriodOrders = await orders.filter(order => {
+    let orderDate = new Date(order.createdAt);
+    return orderDate <= previousPeriod && orderDate >= previousPreviousPeriod;
+  });
+
+  const currentPeriodNumberOfOrders = await filteredPreviousPeriodOrders.length;
+
+  const currentPeriodNumberOfProducts =
+    await filteredPreviousPeriodOrders.reduce(
+      (count, order) => (count += order.products.length),
+      0
+    );
+
+  const currentPeriodRevenue = await filteredPreviousPeriodOrders.reduce(
+    (totalRevenue, order) => (totalRevenue += order.grossTotal),
+    0
+  );
+
+  const previousPeriodNumberOfOrders =
+    await filteredPreviousPreviousPeriodOrders.length;
+
+  const previousPeriodNumberOfProducts =
+    await filteredPreviousPreviousPeriodOrders.reduce(
+      (count, order) => (count += order.products.length),
+      0
+    );
+
+  const previousPeriodRevenue =
+    await filteredPreviousPreviousPeriodOrders.reduce(
+      (totalRevenue, order) => (totalRevenue += order.grossTotal),
+      0
+    );
+
+  res.status(200).json({
+    orders,
+
+    periodRevenue: currentPeriodRevenue,
+    periodRevenueRate: currentPeriodRevenue - previousPeriodRevenue,
+    periodOrder: currentPeriodNumberOfOrders,
+    periodOrderRate: currentPeriodNumberOfOrders - previousPeriodNumberOfOrders,
+    periodNumberOfProducts: currentPeriodNumberOfProducts,
+    periodNumberOfProductsRate:
+      currentPeriodNumberOfProducts - previousPeriodNumberOfProducts,
+
+    orderCount: orders.length,
+    revenue: getTotal(orders),
+    monthlyRevenue,
+  });
+};
 
 const createOrder = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  req.body.company = req.user.company;
+  const order = await Order.create(req.body);
+  const sufficientQty = await order.validateQuantity();
+  if (!sufficientQty) {
+    await Order.findOneAndDelete({ _id: order._id });
+    throw new BadRequestError("Insufficient quantity, order failed to create");
+  }
+  await order.updateInventory();
 
-    req.body.createdBy = req.user.userId
-    req.body.company = req.user.company
-    const order = await Order.create(req.body)
-    const sufficientQty = await order.validateQuantity()
-    if (!sufficientQty) {
-        await Order.findOneAndDelete({ _id: order._id })
-        throw new BadRequestError('Insufficient quantity, order failed to create')
-    }
-    await order.updateInventory()
+  res.status(201).json({ order });
+  // res.send('added order')
+};
 
-
-    res.status(201).json({ order })
-    // res.send('added order')
-}
-
-
-
-module.exports = { getAllOrders, createOrder }
+module.exports = { getAllOrders, createOrder };
