e9b3135315b22cd0181a62d04d639f155516e501
kendricktty <79306624+kendricktty@users.noreply.github.com>
2022-07-22 12:55:11 +0800

    Updated logo name in SideNav: SIMPLYLOGS

--- inventoryapp-frontend/src/components/SideNav.js
+++ inventoryapp-frontend/src/components/SideNav.js
@@ -4,8 +4,8 @@ export default function SideNav(props) {
   return (
     <div className="sidebar">
       <div className="logo-details">
-        <img src={"../../logo.jpg"} width="42" height="42" />
-        <div className="logo_name">Simply Logs</div>
+        <img src={"../../logo-rounded.png"} width="42" height="42" />
+        <div className="logo_name">SIMPLYLOGS</div>
       </div>
       <ul className="nav-links">
         <li>
