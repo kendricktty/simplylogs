935767cf9db4360c522bed4caa7a5ca02d214ded
Jing kai Ooi <ooijingkai@gmail.com>
2022-07-25 14:21:47 +0800

    c

--- client/src/axios/axios.js
+++ client/src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 axios.defaults.headers.common["Authorization"] =  `Bearer ${localStorage.getItem('token')}`
 const instance = axios.create({
-    baseURL: "http://localhost:8001" 
+    baseURL: "https://simplylogs-app.herokuapp.com/" 
 })
 export default instance
